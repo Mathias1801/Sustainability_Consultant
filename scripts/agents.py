e8e9edcba922a7b23832d0a3050aba8d72337f32
Esben922 <TR67NH@student.aau.dk>
2025-04-09 11:50:42 +0200

    Update agents.py

--- scripts/agents.py
+++ scripts/agents.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 
 class CustomAgents:
     def __init__(self):
-        self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+        self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.8)
 
     def summarize_agent(self):
         return Agent(
