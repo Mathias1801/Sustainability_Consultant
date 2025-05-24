fcbbc7ec0f18d3ebf33ca2d117fbeaab9cd5a163
Esben922 <TR67NH@student.aau.dk>
2025-05-24 15:09:29 +0200

    rename

--- scripts/llm_utils.py
+++ scripts/llm_utils.py
@@ -1,10 +1,10 @@
 import os
 from google import genai
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 MODEL_ID = "gemini-2.5-flash-preview-05-20"
 
-client = genai.Client(api_key=GOOGLE_API_KEY)
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 def get_gpt():
     return client.models.get(MODEL_ID)
